3068e557546d021d52b464a78f8f0d43c657bd50
Aniketdhage <aniketdhage90@gmail.com>
2022-01-22 15:50:09 +0530

    added pages and linked to router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Navs from "./components/Navs";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Starred from "./Pages/Starred";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/home">
-        This Is Home Page
-      </Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
