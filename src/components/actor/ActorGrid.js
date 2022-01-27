99a24c8670ca23a1f805ae078184d9337cc4aefa
Aniketdhage <aniketdhage90@gmail.com>
2022-01-27 18:42:54 +0530

    installed styled components and created styles for showcard

--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -1,7 +1,22 @@
 import React from "react";
-
-const ActorGrid = () => {
-  return <div>ActorGrid</div>;
+import ActorCard from "./ActorCard";
+import IMAGE_NOT_FOUND from "../../images/not-found.png";
+import { FlexGrid } from "../Styled";
+const ActorGrid = ({ data }) => {
+  return;
+  <FlexGrid>
+    {data.map(({ person }) => {
+      <ActorCard
+        key={person.id}
+        name={person.name}
+        country={person.country ? person.country.name : null}
+        birthday={person.birthday}
+        deathday={person.deathday}
+        gender={person.gender}
+        image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
+      />;
+    })}
+  </FlexGrid>;
 };
 
 export default ActorGrid;
