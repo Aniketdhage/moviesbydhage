1d223787b215de7cde2b4358ea7856d4e398e86a
Aniketdhage <aniketdhage90@gmail.com>
2022-01-31 13:02:05 +0530

    added styled components with amazing theme

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
+import { ThemeProvider } from "styled-components";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 import Show from "./Pages/Show";
 import Starred from "./Pages/Starred";
 
+const theme = {
+  mainColors: {
+    blue: "#2400ff",
+    gray: "#c6c6c6",
+    dark: "#353535",
+  },
+};
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/starred">
-        <Starred />
-      </Route>
-      <Route exact path="/about">
-        <About />
-      </Route>
-      <Route exact path="/show/:id">
-        <Show />
-      </Route>
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
+import { NavList, LinkStyled } from "./nav.styled";
 
 const LINKS = [
   {
@@ -17,15 +18,21 @@ const LINKS = [
 ];
 
 const Navs = () => {
+  const location = useLocation();
   return (
     <div>
-      <ul>
+      <NavList>
         {LINKS.map((item, index) => (
           <li key={index}>
-            <Link to={item.to}>{item.text}</Link>
+            <LinkStyled
+              to={item.to}
+              className={item.to === location.pathname ? "active" : ""}
+            >
+              {item.text}
+            </LinkStyled>
           </li>
         ))}
-      </ul>
+      </NavList>
     </div>
   );
 };
